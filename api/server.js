e794986690311c427bccc10558f4a9051e2c8861
SDHZed <83025970+sdhz980@users.noreply.github.com>
2024-03-21 05:33:09 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
+const fs = require("fs")
+const path = require("path")
+const db = JSON.parse(fs.readFileSync(path.join("db.json")))
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
